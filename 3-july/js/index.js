f717df1f0fca131edbefb1b6c63644645ceffef8
Official-Akash <akashverma141997@gmail.com>
2018-07-03 12:34:06 +0530

    change 3

--- 3-july/js/index.js
+++ 3-july/js/index.js
@@ -1,13 +1,50 @@
 //onload
+//var a = prompt("Enter a number");
+//alert("You have entered "+a);
+
+if(confirm("Yes/No?")){
+  alert("hello");
+}else{
+  window.location.reload();
+}
+
+//on element only
 
 //onclick
-
-//onchange
+function imagecaption(){
+  var cap = document.getElementById("one");
+  //display none to block
+  cap.style.display = "block";
+}
 
 //onmouseover
+function thiscap(){
+  var cap2 = document.getElementById("two");
+  //display none to block
+  cap2.style.display = "block";
+}
 
 //onmouseout
+function remthiscap(){
+  var cap3 = document.getElementById("two");
+  //display none to block
+  cap3.style.display = "none";
+}
+
+//onchange
+function errr(){
+  console.log("changed");
+}
 
 //onkeydown
+function err2(){
+  console.log("changed");
+}
 
 //addeventlistener
+/*document.addEventListener("click", anonFun);
+function anonFun(){
+  window.alert("Hello World");
+  //remove eventlistener
+  document.removeEventListener("click", anonFun);
+}*/
