a6895297bbd820272de98772723380bb5ee56ccf
Official-Akash <akashverma141997@gmail.com>
2018-07-06 12:25:37 +0530

    change 2

---
+++ 6-july/js/index.js
@@ -0,0 +1,51 @@
+// $(document).ready(function(){
+$(function(){
+
+  //jQuery Code goes here.........
+  $("button[name=button]").click(function(){
+    //console.log($(this).text("Clicked"));
+
+    //hide()
+    // $("#para").hide(2000, function(){
+    //   console.log("Hide");
+    // });
+
+    //show()
+    //$("#para").show();
+
+    //toggle()
+    //$("#para").toggle();
+
+    //slideUp();
+    //$("#para").slideUp();
+
+    //slideDown();
+    //$("#para").slideDown();
+
+    //slideToggle()
+    //$("#para").slideToggle();
+
+    //fadeIn()
+    //$("#para").fadeIn();
+
+    //fadeOut()
+    //$("#para").fadeOut();
+
+    //fadeToggle()
+    //$("#para").fadeToggle();
+
+    //fadeTo()
+    //$("#para").fadeTo(1000, 0.5);
+
+    //animate()
+    // $("#para").animate({
+    //   opacity: 0.4,
+    //   marginLeft: "0.6in",
+    //   fontSize: "3em",
+    //   borderWidth: "10px"
+    // });
+
+    //html()
+    console.log($(".main").html());
+  });
+});
