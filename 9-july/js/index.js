40e259e6a4004eac8787f4b25b665580f2c84edc
Official-Akash <akashverma141997@gmail.com>
2018-07-09 13:43:49 +0530

    data added

---
+++ 9-july/js/index.js
@@ -0,0 +1,31 @@
+$(function(){
+  //append and prepend
+  // var a = "<p id=\"abc\">Lorem</p>";
+  // var b = document.createElement("p");
+  // b.innerHTML = "Lorem";
+  // $('body').append(a, b);
+  // $('body').prepend(a);
+
+  //after and before
+  // $('p').after("Hello");
+  // $('p').before("Hello");
+
+  //remove and empty
+  // $('button').click(function(){
+  //   $('div').remove();
+  //   $('div').empty();
+  // });
+
+  //addClass, removeClass and ToggleClass
+  //$('button').click(function(){
+    //$('div').addClass('colo');
+    //$('div').removeClass('colo');
+    //$('div').toggleClass('colo');
+  //});
+
+  //css
+  // $('button').click(function(){
+  //   $('div').css({'color':'red', 'font-size':'40px'});
+  // });
+  
+});
