3d03d5a83adf7801a50a3c9f17c402eeacb78d55
Official-Akash <akashverma141997@gmail.com>
2018-07-18 12:24:30 +0530

    more data added

---
+++ Templates/Assets/js/index.js
@@ -0,0 +1,9 @@
+function myFunction(y) {
+    y.classList.toggle("change");
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
